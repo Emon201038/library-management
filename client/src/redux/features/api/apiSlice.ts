cd6edb58cb7c451e8e397788ded8ba7e3e4a6caf
Emon201038 <emdadul2580@gmail.com>
2025-07-05 11:37:18 +0600

    Update API base URL to use production endpoint

--- client/src/redux/features/api/apiSlice.ts
+++ client/src/redux/features/api/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://ph-assignment-4.onrender.com/api' }),
 
   endpoints: () => ({}),
 });
